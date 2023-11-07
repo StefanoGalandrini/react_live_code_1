ad3bcebcd09208c321fc485752b3569c14a98218
leicaflorian <florian.leica@gmail.com>
2023-11-07 11:12:36 +0100

    adds nodemon e dotenv

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const http = require("http");
 
+// importo la libreria dotenv
+const dotenv = require("dotenv");
+
+// la avvio
+dotenv.config();
+
+// require("dotenv").config();
+
 const port = process.env.PORT || 3000;
 
 // Creo il server
 const server = http.createServer(function (req, res) {
   // Devo specificare come il server deve rispondere
   // res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello World!</h1>");
+  res.end(`<h1>Hello Classe #1 Node / React!</h1>
+  PIN: ${process.env.PIN}<br>
+  CVV: ${process.env.CVV}
+  `);
 })
 
 // avvio il server
